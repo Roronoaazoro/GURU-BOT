5f79532d6c06cb45c24035f1c600ab4b6a89df87
Roronoaazoro <142931948+Roronoaazoro@users.noreply.github.com>
2023-10-01 16:53:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
+  
+  ['18056331865', 'zoro', true], 
+   
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'AK bot'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.packname = 'AK┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
